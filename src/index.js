f9a58e43b25985187f2e0a0c86905391bbbdebc4
Halyna Pryn <prynhalyna@gmail.com>
2021-01-19 14:21:36 +0100

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,34 +1,43 @@
 import './style.css';
-import debounce from 'lodash.debounce';
+
 import updateMarkup from './js/update';
 import refs from './js/refs';
 import serviceApi from './js/serviceApi';
 
-refs.formRef.addEventListener('input', debounce(getsImage, 500));
+console.log(window);
 
-function getsImage(e) {
-  e.preventDefault();
-  serviceApi.query = e.target.value;
+refs.formRef.addEventListener('submit', event => {
+  event.preventDefault();
+  const form = event.currentTarget;
+  serviceApi.query = form.elements.query.value;
 
+  refs.list.innerHTML = '';
   serviceApi.resetPage();
+
   refs.btnLoadMore.classList.add('is-hidden');
+
   serviceApi.fetchApi().then(hits => {
     updateMarkup(hits);
     refs.btnLoadMore.classList.remove('is-hidden');
+
+    window.scrollTo({
+      top: document.documentElement.offsetHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
   });
-}
+});
 
 refs.btnLoadMore.addEventListener('click', onLoadMoreBtnClick);
 
 function onLoadMoreBtnClick() {
   serviceApi.fetchApi().then(hits => {
     updateMarkup(hits);
-    refs.btnLoadMore.classList.remove('is-hidden');
-  });
 
-  window.scrollBy({
-    top: window.innerHeight,
-    left: 0,
-    behavior: 'smooth',
+    window.scrollTo({
+      top: document.documentElement.offsetHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
   });
 }
